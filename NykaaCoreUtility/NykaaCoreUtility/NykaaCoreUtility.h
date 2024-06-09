203757f9f214a19cb8e0dfde6ccccb379ba02595
Pravin Sankeshwari <pravin.sankeshwari@nykaa.com>
2024-06-09 16:07:15 +0530

    added iCarousel

---
+++ NykaaCoreUtility/NykaaCoreUtility/NykaaCoreUtility.h
@@ -0,0 +1,18 @@
+//
+//  NykaaCoreUtility.h
+//  NykaaCoreUtility
+//
+//  Created by Pravin Sankeshwari on 09/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NykaaCoreUtility.
+FOUNDATION_EXPORT double NykaaCoreUtilityVersionNumber;
+
+//! Project version string for NykaaCoreUtility.
+FOUNDATION_EXPORT const unsigned char NykaaCoreUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NykaaCoreUtility/PublicHeader.h>
+
+
